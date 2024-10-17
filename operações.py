17371e1fb325ad866f7bdc15f333a58bdbc7c0b3
Weslley23 <108986832+Weslley23@users.noreply.github.com>
2024-10-17 16:48:56 -0300

    Add files via upload

---
+++ operações.py
@@ -0,0 +1,9 @@
+'''
+Faça um algoritimo para ler dois números e imprimir a soma dos números lidos.
+'''
+
+# Entre com o primeiro número: 10
+n1 = int(input("Entre com o primeiro número: "))
+n2 = int(input("Entre com o segundo número: "))
+soma = n1 + n2
+print(soma)
