313df668297229112f422ed4247d200bba87500b
weslley e <weslleyevg23@gmail.com>
2024-10-21 15:21:05 -0300

    atualização

---
+++ WESLLEY PYTHON/saque.py
@@ -0,0 +1,12 @@
+'''
+Receba o valor do saque e compare com o saldo da conta bancaria
+'''
+saldo_inicial = 1000
+saldo = saldo_inicial
+saque = float(input('Digite o valor do saque:' ))
+if saque <= saldo_inicial: 
+    print('Saque realizado com sucesso')
+    saldo = saldo_inicial - saque
+else:
+     print('Saldo insuficiente')
+print(f'Saldo atual: {saldo}')
